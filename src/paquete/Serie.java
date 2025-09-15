61c0b6ab2367728684e149c7b895a65c87739af5
Ignacio <Ignaciorubenm12@gmail.com>
2025-09-15 18:04:19 -0300

    Improve Javadoc for paquete classes

--- src/paquete/Serie.java
+++ src/paquete/Serie.java
@@ -1,5 +1,8 @@
 package paquete;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * La clase Serie representa un tipo de Contenido compuesto por varias temporadas.
  * Cada temporada puede incluir distintos episodios u otras características
@@ -13,9 +16,6 @@ package paquete;
  * @author grupo32
  * @version 1.0
  */
-
- import java.util.ArrayList;
-import java.util.List;
 public class Serie extends Contenido {
 
     private List<Temporada> temporadas;   // Lista de temporadas
@@ -29,7 +29,19 @@ public class Serie extends Contenido {
     }
 
     // --- Getters & Setters ---
+
+    /**
+     * Obtiene la lista de temporadas que componen la serie.
+     *
+     * @return lista de objetos {@link Temporada} registrados.
+     */
     public List<Temporada> getTemporadas() { return temporadas; }
+
+    /**
+     * Reemplaza la lista de temporadas asociada a la serie.
+     *
+     * @param temporadas colección de temporadas que describen la serie.
+     */
     public void setTemporadas(List<Temporada> temporadas) {
         this.temporadas = temporadas;
     }
---
+++ src/paquete/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Proporciona las clases del dominio para la plataforma de streaming de ejemplo.
+ * Incluye representaciones de contenidos audiovisuales, catálogos y temporadas.
+ */
+package paquete;
