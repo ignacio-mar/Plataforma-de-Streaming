971a79ac6ca951ce03b5bffdd8f2a98433949cd1
Ignacio <Ignaciorubenm12@gmail.com>
2025-10-20 16:55:24 -0300

    fede

--- src/paquete/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Proporciona las clases del dominio para la plataforma de streaming de ejemplo.
- * Incluye representaciones de contenidos audiovisuales, catálogos y temporadas.
- */
-package paquete;
